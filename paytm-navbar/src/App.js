c8542b7fdf7bad20218ff1425a9f9a6a5e88b9fc
Mayank <kaushalmayank.k@gmail.com>
2022-12-29 23:39:24 +0530

    Tailwind setup done

---
+++ paytm-navbar/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav className='bg-green-800 w-full'>
+      <img src="" alt="" />
+      <ul>
+        <li>Paytm for Consumer</li>
+        <li>Paytm for Business</li>
+        <li>Investir Relations</li>
+        <li>Company</li>
+        <li>Career</li>
+      </ul>
+      <button>
+
+      </button>
+    </nav>
+    </>
+  );
+}
+
+export default App;
