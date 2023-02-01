bef9bcc6f2e99903f49e1067e88ee3ad0ae6a1e4
Mayank <kaushalmayank.k@gmail.com>
2023-02-02 00:24:25 +0530

    assignnment complete

--- firstAPI/index.js
+++ firstAPI/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express()
+const restaurants = require('./restaurantdata.json')
+const mealtypes = require('./mealtype.json')
+// console.log('restaurants: ', restaurants);
+// console.log('mealtypes: ', mealtypes)
 
 
-app.get('/:name', (req,res) => {
-  res.send('jello' + req.params.name)
+const placeFilter = (place) => {
+  return restaurants.filter(rstr => rstr.city_name.toLowerCase() === place.toLowerCase())  
+}
+
+app.get('/search/:city_name', (req,res) => {
+  res.send(placeFilter(req.params.city_name));
+})
+
+app.get('/mealtypes' , (req,res) => {
+  res.send(mealtypes)
 })
 
 app.listen(3000)
