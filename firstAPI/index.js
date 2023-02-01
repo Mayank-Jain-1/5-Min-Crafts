aa002938107d348dd3be56539f83b11a912bf6c6
Mayank <kaushalmayank.k@gmail.com>
2023-02-02 00:34:14 +0530

    used fs to convert to read txt files

--- firstAPI/index.js
+++ firstAPI/index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const app = express()
-const restaurants = require('./restaurantdata.json')
-const mealtypes = require('./mealtype.json')
-// console.log('restaurants: ', restaurants);
-// console.log('mealtypes: ', mealtypes)
-
+const express = require("express");
+const fs = require("fs");
+const restaurants = JSON.parse(fs.readFileSync("./restaurantdata.txt", "utf8"));
+const mealtypes = JSON.parse(fs.readFileSync("./restaurantdata.txt", "utf8"));
 
 const placeFilter = (place) => {
-  return restaurants.filter(rstr => rstr.city_name.toLowerCase() === place.toLowerCase())  
-}
+  return restaurants.filter(
+    (rstr) => rstr.city_name.toLowerCase() === place.toLowerCase()
+  );
+};
 
-app.get('/search/:city_name', (req,res) => {
+const app = express();
+app.get("/search/:city_name", (req, res) => {
   res.send(placeFilter(req.params.city_name));
-})
-
-app.get('/mealtypes' , (req,res) => {
-  res.send(mealtypes)
-})
-
-app.listen(3000)
+});
+app.get("/mealtypes", (req, res) => {
+  res.send(mealtypes);
+});
+app.listen(3000);
