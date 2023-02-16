1737a93a34a12f3f159dbe18a45d62c8a70eb9a6
Mayank <kaushalmayank.k@gmail.com>
2023-02-16 18:30:37 +0530

    Assignment 6 complete

--- firstAPI/index.js
+++ firstAPI/index.js
@@ -10,10 +10,78 @@ const placeFilter = (place) => {
 };
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send(restaurants);
+});
+
 app.get("/search/:city_name", (req, res) => {
   res.send(placeFilter(req.params.city_name));
 });
 app.get("/mealtypes", (req, res) => {
   res.send(mealtypes);
 });
+
+app.get("/filter", (req, res) => {
+  const defaultFilter = {
+    cuisinesarr: null,
+    minCost: 0,
+    maxCost: Number.MAX_VALUE,
+    page: 1,
+    pageSize: 4,
+  };
+
+  let { cuisinesarr, cost, sort, page, pageSize } = req.query;
+  cuisinesarr = cuisinesarr
+    ? cuisinesarr.toLowerCase().split("-")
+    : defaultFilter.cuisinesarr;
+
+  let minCost = defaultFilter.minCost,
+    maxCost = defaultFilter.maxCost;
+  page = page ? page : defaultFilter.page;
+  pageSize = pageSize ? pageSize : defaultFilter.pageSize;
+
+  if (cost) {
+    switch (cost[0]) {
+      case "<":
+        minCost = 0;
+        maxCost = parseInt(cost.slice(1));
+        break;
+      case ">":
+        minCost = parseInt(cost.slice(1));
+        maxCost = Number.MAX_VALUE;
+        break;
+      default:
+        minCost = parseInt(cost.split("-")[0]);
+        maxCost = parseInt(cost.split("-")[1]);
+    }
+  }
+
+  const resultData = restaurants.filter((restaurant) => {
+    return (
+      (!cuisinesarr ||
+        restaurant.Cuisine.some((cuisine) =>
+          cuisinesarr.includes(cuisine.name.toLowerCase())
+        )) &&
+      minCost <= restaurant.cost &&
+      restaurant.cost <= maxCost
+    );
+  });
+
+  switch (sort) {
+    case "low":
+      resultData.sort((a, b) => a.cost - b.cost);
+      break;
+    case "high":
+      resultData.sort((a, b) => b.cost - a.cost);
+      break;
+    default:
+      break;
+  }
+
+  res.send(resultData.slice((page - 1) * pageSize, page * pageSize));
+
+  // res.send({ cuisinesarr, minCost, maxCost, sort, page });
+});
+
 app.listen(3000);
